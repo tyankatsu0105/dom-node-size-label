ccc3d9016841a7937f77a3a4ebb9dd8d494c53b0
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-06-24 22:08:56 +0900

    feat: wip display label in browser

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,12 +1,12 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-chrome.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion);
-});
+// chrome.runtime.onInstalled.addListener((details) => {
+//   console.log('previousVersion', details.previousVersion);
+// });
 
-chrome.browserAction.setBadgeText({
-  text: `'Allo`
-});
+// chrome.tabs.onUpdated.addListener((tabId) => {
+//   chrome.pageAction.show(tabId);
+// });
 
-console.log(`'Allo 'Allo! Event Page for Browser Action`);
+// console.log(`'Allo 'Allo! Event Page for Page Action`);
--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,4 +1,11 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-console.log(`'Allo 'Allo! Content script`);
+const element:HTMLDivElement = document.createElement('div');
+const nodeSize:number = document.querySelectorAll('*').length;
+
+element.id = 'dom-node-size-label';
+element.setAttribute('style', 'position: fixed; z-index: 1000; top: 0; left: 0; padding: 2px; font-size: 16px; color: #fff; background-color: #000;')
+
+element.textContent = `Node size : ${nodeSize}`;
+document.body.appendChild(element);
